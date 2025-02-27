9a44c3540d710039133c27fdc404732b7e7e9117
Daniel Santos Martinez <danielsm535@gmail.com>
2025-02-26 21:50:22 -0500

    further changes

--- src/links/Footer.js
+++ src/links/Footer.js
@@ -5,23 +5,31 @@ import {
     faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 
-import "../App.css"
+import "../App.css";
 
 export default function Footer() {
     return (
-        <div className="social-icons">
-            <a href="https://x.com/hibiya_wav" className="twitter">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-            </a>
-            <a
-                href="https://www.instagram.com/hibiya.wav/"
-                className="instagram"
-            >
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-            </a>
-            <a href="https://github.com/hibiya-wav" className="github">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
+        <div>
+            <div className="letter">
+                <h4>
+                    If you would like to contribute additional sites, feel free
+                    to leave a pull request on Github.
+                </h4>
+            </div>
+            <div className="social-icons">
+                <a href="https://x.com/hibiya_wav" className="twitter">
+                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a
+                    href="https://www.instagram.com/hibiya.wav/"
+                    className="instagram"
+                >
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                <a href="https://github.com/hibiya-wav" className="github">
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
+            </div>
         </div>
     );
 }
